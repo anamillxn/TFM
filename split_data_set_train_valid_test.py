923c622653452e3dcc96cb69a486545148f6fa82
anamillxn <anitamillanserrano@gmail.com>
2024-06-12 21:59:35 +0200

    UPDATE

--- split_data_set_train_valid_test.py
+++ split_data_set_train_valid_test.py
@@ -3,14 +3,14 @@ import shutil
 from random import shuffle
 
 # Rutas a las carpetas donde se encuentran todas las imágenes y etiquetas
-carpeta_imagenes = '/home/ana/JSON2YOLO/new_dir/images'
-carpeta_labels = '/home/ana/JSON2YOLO/new_dir/labels'
+carpeta_imagenes = '/home/ana/TFM/datasets/dataset_precrop_sahi_coco'
+carpeta_labels = '/home/ana/JSON2YOLO/new_dir/labels/crop_cell_128_sliced_coco.json_coco'
 
 # Crea las carpetas para los conjuntos de datos
 sets = ['train', 'valid', 'test']
 for set_name in sets:
-    os.makedirs(f'/home/ana/TFM/datasets/dataset_sliced_128_128_YOLO_ampliado/{set_name}/images', exist_ok=True)
-    os.makedirs(f'/home/ana/TFM/datasets/dataset_sliced_128_128_YOLO_ampliado/{set_name}/labels', exist_ok=True)
+    os.makedirs(f'/home/ana/TFM/datasets/dataset_precrop_sahi_yolo/{set_name}/images', exist_ok=True)
+    os.makedirs(f'/home/ana/TFM/datasets/dataset_precrop_sahi_yolo/{set_name}/labels', exist_ok=True)
 
 # Lista todas las imágenes y etiquetas
 all_images = [f for f in os.listdir(carpeta_imagenes) if f.endswith(('.jpg', '.jpeg', '.png'))]
@@ -31,9 +31,9 @@ test_files = all_files[12000:14000]
 def move_files(files, set_name):
     for img, label in files:
         # Mover imágenes
-        shutil.move(os.path.join(carpeta_imagenes, img), f'/home/ana/TFM/datasets/dataset_sliced_128_128_YOLO_ampliado/{set_name}/images/{img}')
+        shutil.move(os.path.join(carpeta_imagenes, img), f'/home/ana/TFM/datasets/dataset_precrop_sahi_yolo/{set_name}/images/{img}')
         # Mover etiquetas
-        shutil.move(os.path.join(carpeta_labels, label), f'/home/ana/TFM/datasets/dataset_sliced_128_128_YOLO_ampliado/{set_name}/labels/{label}')
+        shutil.move(os.path.join(carpeta_labels, label), f'/home/ana/TFM/datasets/dataset_precrop_sahi_yolo/{set_name}/labels/{label}')
 
 # Mueve los archivos a las carpetas correspondientes
 move_files(train_files, 'train')
