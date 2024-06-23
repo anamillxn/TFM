81f547c969f90bebd34bbb6faf45a82a7b69bdd4
anamillxn <anitamillanserrano@gmail.com>
2024-06-23 16:41:07 +0200

    s3 carga modelos

--- docker_proyect/inferencia.py
+++ docker_proyect/inferencia.py
@@ -1,27 +1,49 @@
-from ultralytics import YOLO, RTDETR
-from PIL import Image
+import argparse
+import io
+import boto3
 import numpy as np
+from PIL import Image
+from ultralytics import YOLO, RTDETR
 from sahi import AutoDetectionModel
 from sahi.predict import get_sliced_prediction
 
 class MobileDamageDetection:
-    def __init__(self, yolov9_model_path, rtdetr_model_path, device='cuda:0'):
-        # Carga del modelo YOLO
-        self.yolo_model = YOLO(yolov9_model_path)
-        # Carga del modelo RTDETR
-        self.rtdetr_model = AutoDetectionModel.from_pretrained(
-            model_type='rtdetr',
-            model_path=rtdetr_model_path,
-            confidence_threshold=0.3,
-            device=device
-        )
+    def __init__(self, bucket, yolo_model_key, rtdetr_model_key, device='cuda:0'):
+        self.bucket = bucket
+        self.yolo_model_key = yolo_model_key
+        self.rtdetr_model_key = rtdetr_model_key
+        self.device = device
+        # Carga de modelos
+        self.yolo_model = self.load_model_from_s3(self.yolo_model_key, YOLO)
+        self.rtdetr_model = self.load_model_from_s3(self.rtdetr_model_key, AutoDetectionModel)
 
-    def load_image(self, image_path):
-        # Carga de la imagen
-        return Image.open(image_path)
+    def load_model_from_s3(self, model_key, model_class):
+        """Carga un modelo desde un bucket de S3"""
+        s3_client = boto3.client('s3')
+        response = s3_client.get_object(Bucket=self.bucket, Key=model_key)
+        model_data = response['Body'].read()
+        model_path = f"/tmp/{model_key}"
+        with open(model_path, 'wb') as f:
+            f.write(model_data)
+        if model_class == YOLO:
+            return model_class(model_path)
+        else:
+            return model_class.from_pretrained(
+                model_type='rtdetr',
+                model_path=model_path,
+                confidence_threshold=0.3,
+                device=self.device
+            )
+
+    def load_image_from_s3(self, bucket, key):
+        """Carga una imagen desde un bucket de S3"""
+        s3_client = boto3.client('s3')
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        image_content = response['Body'].read()
+        image = Image.open(io.BytesIO(image_content))
+        return image
 
     def predict_yolo(self, image):
-        # Inferencia del modelo YOLO sobre la imagen
         results = self.yolo_model(image, verbose=False)
         for result in results:
             boxes = result.boxes.cpu().numpy().xyxy
@@ -29,14 +51,12 @@ class MobileDamageDetection:
             names = result.names
             for box, cls in zip(boxes, classes):
                 if names[cls] == "cell phone":
-                    # Recorte y redimensionamiento de la imagen del teléfono celular
                     crop = image.crop((box[0], box[1], box[2], box[3]))
                     new_size = (int(crop.size[0] / 3), int(crop.size[1] / 3))
                     return crop.resize(new_size)
         return None
 
     def predict_rtdetr(self, image_array):
-        # Inferencia del modelo RTDETR sobre la imagen recortada
         return get_sliced_prediction(
             image_array,
             self.rtdetr_model,
@@ -47,20 +67,29 @@ class MobileDamageDetection:
         )
 
     def save_results(self, result, export_dir="demo_data/"):
-        # Exportar resultados visuales
         result.export_visuals(export_dir=export_dir)
         return f"{export_dir}/prediction_visual.png"
 
-# Uso de la clase
-detector = MobileDamageDetection(
-    yolov9_model_path='/home/ana/TFM/modelos/pre-crop-cell-phone/weights/best.pt',
-    rtdetr_model_path='/home/ana/TFM/modelos/experimento_4_ampliado/weights/best.pt'
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some images.')
+    parser.add_argument('s3_bucket', type=str, help='The S3 bucket where the models and image are stored')
+    parser.add_argument('image_key', type=str, help='The S3 key for the image')
+    parser.add_argument('yolo_model_key', type=str, help='S3 key for YOLO model weights')
+    parser.add_argument('rtdetr_model_key', type=str, help='S3 key for RTDETR model weights')
+    args = parser.parse_args()
 
-image = detector.load_image('/home/ana/TFM/datasets/dataset_previa/desperfectos_moviles_reduccion_clases.v1i.yolov9/test/images/IMG_0001-3-_png.rf.929b8b12404c6e6dfc6a0bc00dcd0bb7.jpg')
-cropped_image = detector.predict_yolo(image)
-if cropped_image:
-    image_array = np.array(cropped_image)
-    rtdetr_result = detector.predict_rtdetr(image_array)
-    visual_path = detector.save_results(rtdetr_result)
-    print("Visuals saved at:", visual_path)
+    detector = MobileDamageDetection(
+        bucket=args.s3_bucket,
+        yolo_model_key=args.yolo_model_key,
+        rtdetr_model_key=args.rtdetr_model_key
+    )
+
+    image = detector.load_image_from_s3(args.s3_bucket, args.image_key)
+    cropped_image = detector.predict_yolo(image)
+    if cropped_image:
+        image_array = np.array(cropped_image)
+        rtdetr_result = detector.predict_rtdetr(image_array)
+        visual_path = detector.save_results(rtdetr_result)
+        print("Visuals saved at:", visual_path)
+
+
